e3c07f59ccc7f368679ec06374f9afb8c9649519
Danieleeffe1 <46442854+Danieleeffe1@users.noreply.github.com>
2023-09-27 10:25:52 +0200

    Thumbnails for category

---
+++ modules/aw_easycategories-1.1.community_edition_1_/step1/app/code/local/AW/Easycategories/sql/easycategories_setup/mysql4-install-1.0.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* aheadWorks Co.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://ecommerce.aheadworks.com/AW-LICENSE-COMMUNITY.txt
+ *
+ * =================================================================
+ *                 MAGENTO EDITION USAGE NOTICE
+ * =================================================================
+ * This package designed for Magento COMMUNITY edition
+ * aheadWorks does not guarantee correct work of this extension
+ * on any other Magento edition except Magento COMMUNITY edition.
+ * aheadWorks does not provide extension support in case of
+ * incorrect edition usage.
+ * =================================================================
+ *
+ * @category   AW
+ * @package    AW_Easycategories
+ * @version    1.1
+ * @copyright  Copyright (c) 2010-2012 aheadWorks Co. (http://www.aheadworks.com)
+ * @license    http://ecommerce.aheadworks.com/AW-LICENSE-COMMUNITY.txt
+ */
+
+$installer = $this;
+
+$installer->startSetup();
+
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+
+$setup->addAttribute('catalog_category', 'thumbnail', array(
+                        'group'             => 'General',
+                        'label'             => 'Thumbnail',
+                        'type'              => 'varchar',
+                        'input'             => 'image',
+                        'default'           => '',
+                        'class'             => 'validate-number',
+                        'backend'           => 'catalog/category_attribute_backend_image',
+                        'frontend'          => '',
+                        'source'            => '',
+                        'global'            => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+                        'visible'           => true,
+                        'required'          => false,
+                        'user_defined'      => false,
+                        'searchable'        => false,
+                        'filterable'        => false,
+                        'comparable'        => false,
+                        'visible_on_front'  => false,
+                        'unique'            => false,
+                   ));
+
+$installer->run("");
+$installer->endSetup(); 
